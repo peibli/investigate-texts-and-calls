d39e62afd9da221d3bc5c88a0f444f8270199fad
peibli <lipeibao@126.com>
2017-12-09 15:55:13 +0800

    update print format

--- Task0.py
+++ Task0.py
@@ -1,7 +1,7 @@
 #-*- coding:utf-8 -*- 
 """
-ä¸‹é�¢çš„æ–‡ä»¶å°†ä¼šä»Žcsvæ–‡ä»¶ä¸­è¯»å�–è¯»å�–çŸ­ä¿¡ä¸Žç”µè¯�è®°å½•ï¼Œ
-ä½ å°†åœ¨ä»¥å�Žçš„è¯¾ç¨‹ä¸­äº†è§£æ›´å¤šæœ‰å…³è¯»å�–æ–‡ä»¶çš„çŸ¥è¯†ã€‚
+Ã¤Â¸â€¹Ã©ï¿½Â¢Ã§Å¡â€žÃ¦â€“â€¡Ã¤Â»Â¶Ã¥Â°â€ Ã¤Â¼Å¡Ã¤Â»Å½csvÃ¦â€“â€¡Ã¤Â»Â¶Ã¤Â¸Â­Ã¨Â¯Â»Ã¥ï¿½â€“Ã¨Â¯Â»Ã¥ï¿½â€“Ã§Å¸Â­Ã¤Â¿Â¡Ã¤Â¸Å½Ã§â€�ÂµÃ¨Â¯ï¿½Ã¨Â®Â°Ã¥Â½â€¢Ã¯Â¼Å’
+Ã¤Â½Â Ã¥Â°â€ Ã¥Å“Â¨Ã¤Â»Â¥Ã¥ï¿½Å½Ã§Å¡â€žÃ¨Â¯Â¾Ã§Â¨â€¹Ã¤Â¸Â­Ã¤Âºâ€ Ã¨Â§Â£Ã¦â€ºÂ´Ã¥Â¤Å¡Ã¦Å“â€°Ã¥â€¦Â³Ã¨Â¯Â»Ã¥ï¿½â€“Ã¦â€“â€¡Ã¤Â»Â¶Ã§Å¡â€žÃ§Å¸Â¥Ã¨Â¯â€ Ã£â‚¬â€š
 """
 import csv
 with open('texts.csv', 'r') as f:
@@ -14,13 +14,13 @@ with open('calls.csv', 'r') as f:
 
 
 """
-ä»»åŠ¡0:
-çŸ­ä¿¡è®°å½•çš„ç¬¬ä¸€æ�¡è®°å½•æ˜¯ä»€ä¹ˆï¼Ÿé€šè¯�è®°å½•æœ€å�Žä¸€æ�¡è®°å½•æ˜¯ä»€ä¹ˆï¼Ÿ
-è¾“å‡ºä¿¡æ�¯:
+Ã¤Â»Â»Ã¥Å Â¡0:
+Ã§Å¸Â­Ã¤Â¿Â¡Ã¨Â®Â°Ã¥Â½â€¢Ã§Å¡â€žÃ§Â¬Â¬Ã¤Â¸â‚¬Ã¦ï¿½Â¡Ã¨Â®Â°Ã¥Â½â€¢Ã¦ËœÂ¯Ã¤Â»â‚¬Ã¤Â¹Ë†Ã¯Â¼Å¸Ã©â‚¬Å¡Ã¨Â¯ï¿½Ã¨Â®Â°Ã¥Â½â€¢Ã¦Å“â‚¬Ã¥ï¿½Å½Ã¤Â¸â‚¬Ã¦ï¿½Â¡Ã¨Â®Â°Ã¥Â½â€¢Ã¦ËœÂ¯Ã¤Â»â‚¬Ã¤Â¹Ë†Ã¯Â¼Å¸
+Ã¨Â¾â€œÃ¥â€¡ÂºÃ¤Â¿Â¡Ã¦ï¿½Â¯:
 "First record of texts, <incoming number> texts <answering number> at time <time>"
 "Last record of calls, <incoming number> calls <answering number> at time <time>, lasting <during> seconds"
 """
 
 #Task 0
-print "First record of texts, {} texts {} at time {}".format(texts[0][0], texts[0][1], texts[0][2])
-print "Last record of calls, {} calls {} at time {}, lasting {} seconds".format(calls[-1][0], calls[-1][1], calls[-1][2], calls[-1][3])
+print ("First record of texts, {} texts {} at time {}".format(texts[0][0], texts[0][1], texts[0][2]))
+print ("Last record of calls, {} calls {} at time {}, lasting {} seconds".format(calls[-1][0], calls[-1][1], calls[-1][2], calls[-1][3]))
